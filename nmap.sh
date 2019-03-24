85bf08d2f16fbfa33572c3460e2aeee4a4fd8799
iggysav <45374382+iggysav@users.noreply.github.com>
2019-03-24 15:51:36 +0300

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sudo yum install nmap -y 
+sudo apt-get install nmap -y 
 sudo nmap -sP $1 >> online_hosts.txt
-
-#| grep 192. | awk '{print $NF}' >> online_hosts.txt
-sudo yum remove nmap -y 
+sudo apt-get remove nmap -y 
